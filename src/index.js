2cee3dd1ffcf3a1ea988d4dadd3e7e899dd1d6f6
eugenio.barros.jr@gmail.com <eugenio.barros.jr@gmail.com>
2020-05-12 20:28:19 -0300

    chore: initial config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+
+const App = () => (
+  <View style={{ backgroundColor: '#FAFAFA', flex: 1 }}>
+    <StatusBar barStyle="light-content" backgroundColor="#522E92" />
+  </View>
+);
+
+export default App;
